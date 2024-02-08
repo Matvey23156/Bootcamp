64dd40ef5da694e07bace3f31e4818fb2df7898c
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-02-08 18:06:47 +1000

    func InputArray,func sortVibor

---
+++ bootcamp/selectionSort/Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Сортировка выбором O(n^2)
+
+/*
+// Показательный пример сортиировки выбором
+// Отталкиваемся от минимального элемента
+[45, 23, -10, 5, 3, 9, 1]
+min = -10
+[-10, 23, 45, 5, 3, 9, 1]
+min = 1
+[ -10, 1, 45, 3, 9, 23]
+min = 3
+[-10, 1, 3, 5, 45, 9, 23]
+min = 5
+[-10, 1, 3, 5, 45, 9, 23]
+min = 9
+[-10, 1, 3, 5, 9, 45, 23]
+min = 23
+[-10, 1, 3, 5. 9, 23, 45]
+*/
+
+// Реализация данного метода через функцию ( она будет возвращать отсортированный массив )
+
+int[] sortVibor(int[] array)
+{
+    // Cчетчики цикла лучше всего обзывать i, j, k, m, n
+    for (int i = 0; i < array.Length; i++)
+    {
+        int indexMin = i;
+        for (int j = i; j < array.Length; j++)
+        {
+            if (array[j] < array[indexMin])
+            indexMin = j;
+        }
+        if (array[indexMin] == array[i])
+        continue;
+
+        int temp = array[i];
+        array[i] = array[indexMin];
+        array[indexMin] = temp;
+        
+    }
+
+    return array;
+}
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-20,21); // [-20; 20]
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+System.Console.WriteLine($"Начаальный массив: [{String.Join (", ", array)}]");
+System.Console.WriteLine($"Конечный массив: [{String.Join (", ", sortVibor(array))}]");
---
+++ hwArrayautotest/Task1/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,90].
+
+// Начальные условия:
+
+// int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+// Выводится: 5
+
+
+//   int[] numbers = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+//         int minRange = 10;
+//         int maxRange = 90;
+//         int count = 0;
+//         foreach (int e in numbers)
+//         {
+//             if (e >= minRange && e <= maxRange)
+//             {
+//                 count++;
+//             }
+            
+//         }
+        
+//         System.Console.WriteLine(count);
+
+
+// Задайте массив на 10 целых чисел. Напишите программу, 
+// которая определяет количество чётных чисел в массиве.
+
+// Начальные условия:
+
+// int[] array = {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+// Выводится: 6
+
+// int[] array = {2, 5, 2, 3, 6, 3, 7, 8, 2, 4};
+// int count = 0;
+
+// foreach (int e in array)
+// {
+//     if (e % 2 == 0)
+//     {
+//         count++;
+//     }
+// }
+// System.Console.WriteLine(count);
+
+
+//Задайте массив из вещественных чисел с ненулевой дробной частью. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] numbers = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01};
+double FindMin(double[] numbers)
+{
+    double Min = numbers[0];
+    foreach (double e in numbers)
+    {
+        if (e < Min)
+        {
+            Min = e;
+        }
+        
+    }
+return Min;
+}
+double Min = FindMin(numbers);
+System.Console.WriteLine(Min);
+
+double FindMax(double[] numbers)
+{
+
+    double Max = numbers[0];
+    foreach (double e in numbers)
+    {
+        if (e > Max)
+        {
+            Max = e;
+        }
+    }
+    return Max;
+}
+double Max = FindMax(numbers);
+System.Console.WriteLine(Max);
+System.Console.WriteLine(Max - Min);
